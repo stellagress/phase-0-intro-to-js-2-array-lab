8429c5fbb0fd9e5c39cf2186bf3aed3df19f1695
Stella Mazzillo <stellagress5@gmail.com>
2023-03-22 23:38:46 -0400

    arrays labs

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Write your solution here!
+
+//const { returns } = require("sinon/lib/sinon/fake")
+
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+
+
+
+function destructivelyAppendCat() {
+    return cats.push("Ralph")
+}
+
+
+
+function destructivelyPrependCat() {
+    return cats.unshift("Bob")
+}
+
+
+
+function destructivelyRemoveLastCat(){
+    return cats.pop()
+}
+
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift()
+}
+
+
+
+function appendCat(){
+    const newCats = [...cats, "Broom"]
+    return newCats
+}
+
+
+function prependCat(){
+    const newCats = ["Arnold", ...cats]
+    return newCats
+}
+
+
+function removeLastCat(){
+    return cats.slice(0,2)
+     
+}
+
+
+function removeFirstCat(){
+    return cats.slice(1,3)
+}
